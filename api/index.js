33fadae4e2d1cce3884a197b323ca653ef392e9d
Ahmad-Faqehi <ahmadpsn7@gmail.com>
2021-08-17 15:03:59 +0300

    first command

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { Pool, Client } = require("pg");
+const app = express();
+const port = 3001;
+
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+});
+
+app.get("/data", function (req, res) {
+  // res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Content-type', 'application/json');
+
+  pool.query(
+    "SELECT course, description FROM dojo_courses;",
+    [],
+    (err, result) => {
+      if (err) {
+        return res.status(405).jsonp({
+          error: err,
+        });
+      }
+
+      return res.status(200).jsonp({
+        data: result.rows,
+      });
+    }
+  );
+});
+
+app.listen(port, () =>
+  console.log(`Backend rest api listening on port ${port}!`)
+);
